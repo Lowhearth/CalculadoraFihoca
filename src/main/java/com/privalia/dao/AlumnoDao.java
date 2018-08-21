08c7d56485a81be0e57cc944a74f0ccb54286d76
Lowhearth <hearthisinyourmind@gmail.com>
2018-08-21 15:47:27 +0200

    Refactoring search id method

--- src/main/java/com/privalia/dao/AlumnoDao.java
+++ src/main/java/com/privalia/dao/AlumnoDao.java
@@ -12,36 +12,45 @@ import java.io.IOException;
 
 public class AlumnoDao implements IDao<Alumno>{
 	
-	public static File x = null;
+	public static File file = null;
 	
 	static {
-		x = createFile();
+		file = createFile();
 	}
 
 	
 	public Alumno add(Alumno alumno) throws UnsupportedOperationException, IOException{
 		
-		FileWriter fw = new FileWriter(x.getAbsoluteFile(), true);
+		FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
 		fw.write(String.format("%n"));
 		fw.write(alumno.toString());
 		fw.close();
-		BufferedReader br = new BufferedReader(new FileReader(x));
+		 
+		 
+		return searchById(alumno.getIdAlumno());
+	}
+	
+	private Alumno searchById(int i) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new FileReader(file));
 		
 		String currentLine = "";
-		 while ( br.readLine() != null) {
+		boolean alumnoFound = false;
+		 while ( br.readLine() != null || !alumnoFound) {
 		        currentLine = br.readLine();
+		        String[] data = currentLine.split(",", 4);
+		        if(Integer.parseInt(data[0]) == i) {
+		        	alumnoFound = true;
+		        }
 		 }
 		 br.close();
 		 String[] data = currentLine.split(",", 4);
+		 Alumno foundAlumn = new Alumno();
+		 foundAlumn.setIdAlumno(Integer.parseInt(data[0]));
+		 foundAlumn.setNombre(data[1]);
+		 foundAlumn.setApellidos(data[2]);
+		 foundAlumn.setDni(data[3]);
 		 
-		 Alumno insertedAlumno = new Alumno();
-		 insertedAlumno.setIdAlumno(Integer.parseInt(data[0]));
-		 insertedAlumno.setNombre(data[1]);
-		 insertedAlumno.setApellidos(data[2]);
-		 insertedAlumno.setDni(data[3]);
-		 
-		 
-		 
-		return insertedAlumno;
+		 return foundAlumn;
+		
 	}
 }
