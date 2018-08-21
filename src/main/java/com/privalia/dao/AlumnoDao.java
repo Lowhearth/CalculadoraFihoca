24e9365614791d37ef8dd695e9a9c26499c51851
Lowhearth <hearthisinyourmind@gmail.com>
2018-08-21 15:16:59 +0200

    dao

--- src/main/java/com/privalia/dao/AlumnoDao.java
+++ src/main/java/com/privalia/dao/AlumnoDao.java
@@ -4,7 +4,9 @@ import com.privalia.common.Alumno;
 
 import static com.privalia.util.FileManager.createFile;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -19,6 +21,15 @@ public class AlumnoDao implements IDao<Alumno>{
 		fw.write(String.format("%n"));
 		fw.write(alumno.toString());
 		fw.close();
+		BufferedReader br = new BufferedReader(new FileReader(x));
+		
+		String currentLine = "";
+		 while ( br.readLine() != null) {
+		        currentLine = br.readLine();
+		 }
+		 br.close();
+		 
+		 
 		return alumno;
 	}
 }
