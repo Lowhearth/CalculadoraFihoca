9bac1f79ff7b87ceb3ff1111875334cb6b05e77b
Lowhearth <hearthisinyourmind@gmail.com>
2018-08-23 11:57:52 +0200

    Refactored code

--- src/main/java/com/privalia/dao/JsonAlumnoDAO.java
+++ src/main/java/com/privalia/dao/JsonAlumnoDAO.java
@@ -12,9 +12,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.privalia.common.Alumno;
@@ -29,34 +26,29 @@ public class JsonAlumnoDAO implements IDao<Alumno> {
 	
 	
 	@Override
-	public Alumno add(Alumno model) throws JsonGenerationException, JsonMappingException, IOException {
+	public Alumno add(Alumno model) throws IOException   {
 		
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		
-		
+		try (FileWriter writer = new FileWriter(file.getAbsolutePath())){
 		if(file.length() > 0) {
 			String jsonText = readFile(file);
-			FileWriter writer = new FileWriter(file.getAbsolutePath());
 			List<Alumno> alumnos = new ArrayList<>(Arrays.asList(new Gson().fromJson(jsonText, Alumno[].class)));
 			alumnos.add(model);
-			Alumno[] alumnosArray= (Alumno[]) alumnos.toArray(new Alumno[alumnos.size()]);
-			String alumnosString = gson.toJson(alumnosArray);
+			String alumnosString = gson.toJson(alumnos.toArray(new Alumno[alumnos.size()]));
 			System.out.println("adding");
 			writer.write(alumnosString);
-			writer.close();
-			
-			
-		}
+			}
 		else {
 			Alumno [] alumnosArray = {model};
-			FileWriter writer = new FileWriter(file.getAbsolutePath());
-			System.out.println("adding");
 			String alumnosString = gson.toJson(alumnosArray);
-			System.out.println("converted" + alumnosString);
 			writer.write(alumnosString);
-			System.out.println("added");
 			writer.close();
 		}
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
 		
 		
 		Alumno foundAlumn = searchById(model.getIdAlumno());
@@ -67,10 +59,9 @@ public class JsonAlumnoDAO implements IDao<Alumno> {
 	
 	public Alumno searchById(int id) throws IOException  {
 		
-		ObjectMapper mapper = new ObjectMapper();
+		Gson gson = new Gson();
 		String jsonText = readFile(file);
-		;
-		List<Alumno> alumnos = new ArrayList<>(Arrays.asList(mapper.readValue(jsonText, Alumno[].class)));
+		List<Alumno> alumnos = new ArrayList<>(Arrays.asList(gson.fromJson(jsonText, Alumno[].class)));
 		Alumno foundAlumno = alumnos.stream().filter(alumno -> alumno.getIdAlumno() == id)
 						.findFirst()
 						.get();
