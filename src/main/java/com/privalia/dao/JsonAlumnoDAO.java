d08e5923b4afc39becdc0aee4af0853b3d7885e0
Me <me@Mes-MacBook-Pro.local>
2018-08-23 07:24:15 +0200

    First Sketch JsonADAO

--- src/main/java/com/privalia/dao/JsonAlumnoDAO.java
+++ src/main/java/com/privalia/dao/JsonAlumnoDAO.java
@@ -1,14 +1,22 @@
-
 package com.privalia.dao;
 
 
 
 
 import static com.privalia.util.FileManager.createFile;
-
+import static com.privalia.util.FileReader.readFile;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.privalia.common.Alumno;
 
 public class JsonAlumnoDAO implements IDao<Alumno> {
@@ -21,15 +29,37 @@ public class JsonAlumnoDAO implements IDao<Alumno> {
 	
 	
 	@Override
-	public Alumno add(Alumno model) throws  IOException {
-		throw new UnsupportedOperationException();
+	public Alumno add(Alumno model) throws JsonGenerationException, JsonMappingException, IOException {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		String jsonText = readFile(file);
+		
+		List<Alumno> alumnos = new ArrayList<>(Arrays.asList(mapper.readValue(jsonText, Alumno[].class)));
+		alumnos.add(model);
+		Alumno[] alumnosArray= (Alumno[]) alumnos.toArray(new Alumno[alumnos.size()]);
+		
+		mapper.writeValue(file, alumnosArray);
+		
+		
+		
+		return null;
 	}
+	
 	
 	public Alumno searchById(Alumno model) throws IOException  {
 		
+		String jsonText = readFile(file);
+		ObjectMapper mapper = new ObjectMapper();
+		Alumno[] alumnos = mapper.readValue(jsonText, Alumno[].class);
+		
+		
+		
 		
 		throw new UnsupportedOperationException();
 	}
+	
+	
 	
 	
 
